f221d8d661cdb685e87d617d9c3fb7d41fc2ddb8
dmsapereira <david.pinguim2@hotmail.com>
2019-10-21 12:29:49 +0100

    Hella nice

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -4,13 +4,18 @@ import time
 from math import exp
 
 # minimal proportion of accepted neighbours to change temperature
-ACCEPT_NEIGHBOR_THRESHOLD = 0.1 
-ACCEPTANCE_THRESHOLD = 0.01
-TEMP_MULTIPLIER = 0.9
+ACCEPTANCE_THRESHOLD = 0.005
+ACCEPT_NEIGHBOR_THRESHOLD = 0.05
+N_ITER_GEOMETRIC_MULTIPLIER = 1.05
+N_ITER_LINEAR_CONSTANT = 1000 
+GEO_TEMP_MULTIPLIER = 0.95
+GRADUAL_TEMP_MULTIPLIER =  0.01
+
 
 matrix = helper.readDistanceMatrix("distancias.txt")
 
 class Solution:
+
     def __init__(self):
         self.path = []
         self.cost = 0
@@ -22,6 +27,11 @@ class Solution:
 
     def finishPath(self):
         self.cost += helper.distance(matrix, self.path[-1], self.path[0])
+
+    def chosen_one(self, iteration, temp):
+        self.iteration = iteration
+        self.temp = temp
+
     
 
 
@@ -80,8 +90,8 @@ def create_initial_solution(n):
 def neighbor(solution):
     path = solution.path
 
-    i = random.randint(1, len(path) - 4)
-    j = random.randint(1, len(path) - 2)
+    i = random.randint(0, len(path) - 3)
+    j = random.randint(0, len(path) - 1)
 
     while(i >= j or i + 1 == j):
         i = random.randint(1, len(path) - 4)
@@ -90,7 +100,10 @@ def neighbor(solution):
     path1 = path[0:i + 1]
     path2 = path[i + 1: j + 1]
     path2.reverse()
-    path3 = path[j + 1:]
+    if j == (len(path) - 1):
+        path3 = []
+    else:
+        path3 = path[j + 1:]
     path1.extend(path2)
     path1.extend(path3)
 
@@ -107,19 +120,45 @@ def timetoStop(n_iter, accepted_neighbors):
 
 def should_change_temp(current_n_iter, accepted_neighbors):
     n_accepted_neighbors = current_n_iter * ACCEPT_NEIGHBOR_THRESHOLD
-    print("Minimum neighbors : " + str(n_accepted_neighbors) + ". Current accepted : " + str(accepted_neighbors))
     return  accepted_neighbors >= n_accepted_neighbors
 
-def decay_temp(temp):
-    return temp * TEMP_MULTIPLIER
+def decay_temp(temp, mode):
+    if mode == "GEO":
+        return temp * GEO_TEMP_MULTIPLIER
+    return temp / (1 + GRADUAL_TEMP_MULTIPLIER *temp)
+
+def change_n_iter(n_iter, mode):
+    if mode == "CONST":
+        return n_iter 
+    elif mode == "GEO":
+        return n_iter * N_ITER_GEOMETRIC_MULTIPLIER
+    else:
+        return n_iter + N_ITER_LINEAR_CONSTANT
+
+
+
 
 
 def solve():
+
+    temp = input("Initial Temp (AUTO | <value>) :").strip()
+    if temp == "AUTO":
+        temp = getInitialTemp()
+        print(temp)
+
+    n_iter_mode = input("Iteration number mode (LINEAR | GEO | ONE | SMART | CONST) : ").upper().strip()
+
+    if n_iter_mode == "ONE":
+        temp_mode = "GRADUAL"
+
+    temp_mode = "GEO"
+
     n_iter = 10000
-    current = create_initial_solution(len(matrix[0]))
+    first = create_initial_solution(len(matrix[0]))
+    current = first
     best = current
+    worst = current
     next = 0
-    temp = getInitialTemp()
     startTime = int(round(time.time() * 1000))
 
     while(True):
@@ -132,26 +171,43 @@ def solve():
                 accepted_neighbors += 1
                 if current.cost < best.cost:
                     best = current
-            else:
-                if getProb(d, temp):
+                    best.chosen_one(iteration, temp)
+                elif getProb(d, temp):
                     accepted_neighbors +=1
                     current = next
 
-            if(should_change_temp(n_iter, accepted_neighbors)):
+
+            if worst.cost - next.cost < 0:
+                worst = next
+
+            if(n_iter_mode == "ONE"):
                 break
+            elif(n_iter_mode == "SMART" and should_change_temp(n_iter, accepted_neighbors)):
+                break
+
         if timetoStop(n_iter, accepted_neighbors) :
             print("Time Elapsed : " + str(int(round(time.time() * 1000)) - startTime) + "ms")
-            return best 
+            last = current
+            break 
 
-        temp = decay_temp(temp)
+        if n_iter_mode != ("CONST" or "SMART"):
+            n_iter =  change_n_iter(n_iter, n_iter_mode)
+        temp = decay_temp(temp, temp_mode)
 
+    print("First : " , first.path)
+    print("Last : " , last.path)
+    print("Worst : " , worst.path)
+    print("Best")
+    print("\t Iteration : " , best.iteration)
+    print("\t Temp : " , best.temp)
+    print("\t Path : " , best.path)
+    print("\t Cost : " , best.cost)
+    return best
 
             
 
     
 
 solution = solve()
-print(solution.path)
-print(solution.cost)
 
 
